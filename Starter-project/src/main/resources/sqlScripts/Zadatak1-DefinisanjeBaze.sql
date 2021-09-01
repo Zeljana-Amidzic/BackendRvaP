9356956dac018982c618952bf2c34bd203b7b9fc
Zeljana-Amidzic <zeljana.amidzic@uns.ac.rs>
2021-09-01 21:30:30 +0200

    proba za predaju

--- Starter-project/src/main/resources/sqlScripts/Zadatak1-DefinisanjeBaze.sql
+++ Starter-project/src/main/resources/sqlScripts/Zadatak1-DefinisanjeBaze.sql
@@ -3,11 +3,23 @@ DROP TABLE IF EXISTS departman CASCADE;
 DROP TABLE IF EXISTS student CASCADE;
 DROP TABLE IF EXISTS status CASCADE;
 
+DROP SEQUENCE IF EXISTS fakultet_seq;
+DROP SEQUENCE IF EXISTS departman_seq;
+DROP SEQUENCE IF EXISTS status_seq;
+DROP SEQUENCE IF EXISTS student_seq;
+
 CREATE TABLE fakultet
 (
 	id integer not null,
 	naziv varchar(100) not null,
 	sediste varchar(50) not null
+);
+
+CREATE TABLE status
+(
+	id integer not null,
+	naziv varchar(100) not null,
+	oznaka varchar(10) not null
 );
 
 CREATE TABLE departman
@@ -26,13 +38,6 @@ CREATE TABLE student
 	broj_indeksa varchar(20) not null,
 	status integer not null,
 	departman integer not null
-);
-
-CREATE TABLE status
-(
-	id integer not null,
-	naziv varchar(100) not null,
-	oznaka varchar(10) not null
 );
 
 ALTER TABLE fakultet
